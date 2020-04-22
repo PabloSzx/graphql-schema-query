f4cf33144945ac68185dd029b0ec68d85b788b35
PabloSzx <pablosaez1995@gmail.com>
2020-04-22 18:45:20 -0400

    skip doc validation

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import gql, { DocumentNode } from 'graphql-tag-ts';
 
 export { default as gql, DocumentNode } from 'graphql-tag-ts';
 
+const defaultValidateDoc = process.env.NODE_ENV !== 'production';
+
+const defaultEmptyObject = {};
+
 export default (schema: GraphQLSchema) => {
   const schemaValidationErrors = validateSchema(schema);
   if (schemaValidationErrors.length) {
@@ -15,35 +19,45 @@ export default (schema: GraphQLSchema) => {
   >(
     document: DocumentNode<Data, Variables> | string,
     {
-      variables,
-      context,
+      variables: variableValues,
+      context: contextValue,
+      validateDoc = defaultValidateDoc,
     }: {
       variables?: Variables;
       context?: any;
-    } = {}
+      /**
+       * Validate query/mutation into the schema before executing it
+       *
+       * By default is true in development and in production
+       * is turned off.
+       */
+      validateDoc?: boolean;
+    } = defaultEmptyObject
   ) => {
     if (typeof document === 'string') {
       document = gql(document);
     }
 
-    const documentValidationErrors = validate(schema, document);
+    if (validateDoc) {
+      const documentValidationErrors = validate(schema, document);
 
-    if (documentValidationErrors.length) {
-      throw documentValidationErrors;
+      if (documentValidationErrors.length) {
+        throw documentValidationErrors;
+      }
     }
 
     const { data, errors } = await execute<Data>({
       schema,
       document,
-      variableValues: variables,
-      contextValue: context,
+      variableValues,
+      contextValue,
     });
 
     if (errors?.length) {
       throw errors;
     }
     return (
-      data ??
+      data ||
       (() => {
         throw Error('Data could not be found');
       })()
